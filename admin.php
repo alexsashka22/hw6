fb63277a34438e88531ce7a78fef8450d5a31754
Alexandra <34464143+alexsashka22@users.noreply.github.com>
2018-03-12 18:22:17 +0300

    Update admin.php
    нашла ошибку

--- admin.php
+++ admin.php
@@ -1,14 +1,20 @@
 <?php
-  $outfiles_dir = 'tests/';
-  move_uploaded_file($_FILES['outfile']['tmp_name'], $outfiles_dir)
+ $uploads_dir = "tests";
+ foreach ($_FILES["test"] as $key => $error) {
+     if ($error == UPLOAD_ERR_OK) {
+         $tmp_name = $_FILES["test"]["tmp_name"];
+         $name = basename($_FILES["test"]["name"]);
+         move_uploaded_file($tmp_name, "$uploads_dir/$name");
+     }
+ }
  ?>
 
- <html>
- <body>
-   <h2>Загрузите тест</h2>
-   <form action="admin.php" method="POST" enctype="multipart/form-data">
-     <input type="file" name="outfile" />
-     <input type="submit" value="Отправить" />
-   </form>
- </body>
- </html>
+<html>
+<body>
+  <h2>Загрузите тест</h2>
+  <form action="admin.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="test" />
+    <input type="submit" value="Отправить" />
+  </form>
+</body>
+</html>
