e06007f1621197e27f7660f56ba7eee711e35f1a
Alexandra <34464143+alexsashka22@users.noreply.github.com>
2018-04-04 19:07:30 +0300

    Доработанный вариант

--- admin.php
+++ admin.php
@@ -1,14 +1,37 @@
 <?php
- $uploads_dir = "tests";
- foreach ($_FILES["test"] as $error) {
-     if ($error == UPLOAD_ERR_OK) {
-         $tmp_name = $_FILES["test"]["tmp_name"];
-         $name = basename($_FILES["test"]["name"]);
-         move_uploaded_file($tmp_name, "$uploads_dir/$name");
-         // echo "<pre>";
-         // var_dump($_FILES);
-     }
- }
+if(isset($_FILES['test']['tmp_name']) && file_exists($_FILES['test']['tmp_name'])){
+	if(array_pop(explode('.', $_FILES['test']['name'])) == 'json' ){
+		if($json_array = json_decode(file_get_contents($_FILES['test']['tmp_name']), true)){
+			$i = 0;
+			while($i < count($json_array)){
+				if(array_key_exists('q', $json_array[$i]) && array_key_exists('var1', $json_array[$i]) && array_key_exists('var2', $json_array[$i]) && array_key_exists('var3', $json_array[$i]) && array_key_exists('var4', $json_array[$i]) && array_key_exists('answer', $json_array[$i])) {
+					$check = 1;
+				}
+				else{
+					$check = 0;
+				}
+				$i++;
+			}
+			if($check == 1){
+				$files = scandir('tests/');
+				$num_test = count($files) - 1;
+				if(move_uploaded_file($_FILES['test']['tmp_name'], 'tests/'. 'test' .$num_test.'.json')){
+					header ('Location:list.php',true,303);
+					exit;
+				}
+			}
+			else{
+				echo '<p style="color:red">Ошибка неверная структура данных</p>';
+			}
+		}
+		else{
+			echo '<p style="color:red">Загруженный файл не содержит данных JSON. Выберите верный файл</p>';
+		}
+	}
+	else{
+		echo 'Неверный формат файла';
+	}
+}
  ?>
 
 <!DOCTYPE html>
